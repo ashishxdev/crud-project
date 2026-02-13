75b20ee17b4d0b38ff4f6d38430e6b5010baa34e
ashishxdev <ashishrana2004@gmail.com>
2026-02-13 10:25:42 +0530

    base_url updated

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BASE_URL || "http://localhost:3000",
+  baseURL: process.env.BASE_URL || "https://crud-project-roan.vercel.app/",
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
